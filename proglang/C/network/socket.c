5ed259eff280fb1711f461cc6a3f34cb1c98e60e
tianqingwang <tianqing.wang@yahoo.com.cn>
2013-09-21 21:43:45 +0800

    c 语言 socket

--- proglang/C/network/socket.c
+++ proglang/C/network/socket.c
@@ -1,77 +1,102 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
+#include <fcntl.h>
 #include "socket.h"
 
-int Create(int domain, int type, int protocol)
+int socket_create(int domain, int type, int protocol=0)
 {
-    int sockfd = -1;
-    sockfd = socket(domain,type,protocol);
-#ifdef DEBUG
-    printf("(%s:%d):sockfd=%d\n",__FILE__,__LINE__,sockfd);
-#endif
-    return sockfd;
+    return socket(domain,type,protocol);
 }
 
-int CreateByIP(char *sIP,short nPort,int domain, int type, int protocol)
-{
-    struct sockaddr_in sockAddr;
-    int sockfd = -1;
-    
-    sockfd = socket(domain,type,protocol);
-    if (sockfd == -1){
-        return sockfd;
-    }
-    
-    memset(&sockAddr,0,sizeof(sockAddr));
-    sockAddr.sin_family = domain;
-    if (sIP == 0){
-        sockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    }
-    else{
-        sockAddr.sin_addr.s_addr = inet_addr(sIP);
-    }
-    sockAddr.sin_port = htons(nPort);
-    
-    
-}
-
-int SetSockOpt(int sockfd,int level,int optname, const void *optval, int optlen)
+int socket_setopt(int sockfd,int level,int optname,const void *optval,int optlen)
 {
     return setsockopt(sockfd,level,optname,optval,optlen);
 }
 
-int Bind(int sockfd,struct sockaddr *pAddr,int nAddrLen)
+int socket_nonblock(int sockfd)
+{
+    int flags;
+    
+    flags = fcntl(sockfd,F_GETFL);
+    if (flags == -1){
+        return -1;
+    }
+    
+    flags |= O_NONBLOCK;
+    if (fcntl(sockfd,F_SETFL,flags) == -1){
+        return -1;
+    }
+    
+    return 0;
+}
+
+int socket_bind(int sockfd,struct sockaddr *pAddr,int nAddrLen)
 {
     return bind(sockfd,pAddr,nAddrLen);
 }
 
-int Listen(int sockfd,int backlog)
+int socket_listen(int sockfd, int backlog=5)
 {
     return listen(sockfd,backlog);
 }
 
-int Connect(int sockfd,struct sockaddr *serv_addr, int nAddrLen)
+int socket_connect(int sockfd,struct sockaddr *serv_addr,int nAddrLen)
 {
     return connect(sockfd,serv_addr,nAddrLen);
 }
 
-int Accept(int sockfd,struct sockaddr *addr, int *nAddrLen)
+int socket_accept(int sockfd,struct sockaddr *addr, int *nAddrLen)
 {
     return accept(sockfd,addr,(socklen_t*)nAddrLen);
 }
 
-int Send(int sockfd,void *pbuf, int nbuf, int flags)
+int socket_send(int sockfd,void *pbuf, int nbuf, int flags = 0)
 {
     return send(sockfd,pbuf,nbuf,flags);
 }
 
-int Receive(int sockfd,void *pbuf, int nbuf, int flags)
+int socket_recv(int sockfd, void *pbuf, int nbuf, int flags = 0)
 {
     return recv(sockfd,pbuf,nbuf,flags);
 }
 
-int Close(int sockfd)
+int socket_close(int sockfd)
 {
     return close(sockfd);
+}
+
+int socket_setup(int nPort)
+{
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    
+    sockfd = socket_create(AF_INET,SOCK_STREAM,0);
+    if (sockfd == -1){
+        return -1;
+    }
+    
+    if (socket_nonblock(sockfd) == -1){
+        return -1;
+    }
+    
+    int reuse = 1;
+    
+    if (socket_setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse)) == -1){
+        return -1;
+    }
+    
+    memset(serv_addr,0,sizeof(sockaddr_in));
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(nPort);    
+
+    if (socket_bind(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr)) == -1){
+        return -1;
+    }
+    
+    if (socket_listen(sockfd) == -1){
+        return -1;
+    }
+    
+    return sockfd;
 }
--- proglang/C/network/socket.h
+++ proglang/C/network/socket.h
@@ -7,20 +7,22 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int Create(int domain=AF_INET, int type=SOCK_STREAM, int protocol=0); /*setup socket*/
-/*create sock and bind to sIP:nPort*/
-int CreateByIP(char *sIP,short nPort,int domain=AF_INET, int type=SOCK_STREAM, int protocol=0);
-/*set socket options*/
-int SetSockOpt(int sockfd,int level,int optname, const void *optval, int optlen);
+int socket_create(int domain, int type, int protocol=0);
+int socket_setopt(int sockfd,int level,int optname,const void *optval,int optlen);
 
-int Bind(int sockfd,struct sockaddr *pAddr,int nAddrLen);
-int Listen(int sockfd,int backlog=5);
-int Connect(int sockfd,struct sockaddr *serv_addr, int nAddrLen);
-int Accept(int sockfd,struct sockaddr *addr, int *nAddrLen);
-/*data operation*/
-int Send(int sockfd,void *pbuf, int nbuf, int flags = 0);
-int Receive(int sockfd,void *pbuf, int nbuf, int flags=0);
+/*set fd as non-blocking*/
+int socket_nonblock(int sockfd);
+int socket_bind(int sockfd,struct sockaddr *pAddr,int nAddrLen);
+int socket_listen(int sockfd, int backlog=5);
 
-int Close(int sockfd);
+int socket_connect(int sockfd,struct sockaddr *serv_addr,int nAddrLen);
+int socket_accept(int sockfd,struct sockaddr *addr, int *nAddrLen);
+
+int socket_send(int sockfd,void *pbuf, int nbuf, int flags = 0);
+int socket_recv(int sockfd, void *pbuf, int nbuf, int flags = 0);
+
+int socket_close(int sockfd);
+
+int socket_setup(int nPort);
 
 #endif
